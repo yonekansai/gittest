cf0ef11fa6bd143c0961e857bbf81901fa9acf9a
[yonekansai] <[yone@kansai-u.ac.jp]>
2015-06-16 14:54:09 +0900

    no message

---
+++ test.c
@@ -0,0 +1,6 @@
+int[] a = new int[1000];
+void setup(){
+	for(int i=0; i<a.length;i++){
+		a[i]=int(random(10));
+	}
+}
---
+++ test/test.pde
@@ -0,0 +1,6 @@
+int[] a = new int[100];
+void setup(){
+	for(int i=0; i<a.length;i++){
+		a[i]=int(random(10));
+	}
+}
